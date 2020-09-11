1eb3d38fa06cad0bf7fdc49ce7ca894ed8329aab
Shubham Saxena <shubham.saxena_cs.ccv19@gla.ac.in>
2020-09-11 16:00:02 +0530

    Assignment Day 4 Q1

---
+++ Assignment-Day-4/Q1-ImageChange/script.js
@@ -0,0 +1,13 @@
+function turnOn() {
+  ele = document.getElementsByClassName("image");
+  ele[0].src = "pic_bulbon.gif";
+}
+function turnOff() {
+  ele = document.getElementsByClassName("image");
+  ele[0].src = "pic_bulboff.gif";
+}
+
+function turnRed() {
+  ele = document.getElementsByClassName("image");
+  ele[0].src = "pic_bulbred.gif";
+}
